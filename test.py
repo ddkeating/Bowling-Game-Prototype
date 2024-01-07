fa7007b490e70dc877219c3fc9b4d98d28b2230c
ddkeating <73508313+ddkeating@users.noreply.github.com>
2024-01-08 12:19:16 +1300

    Corrected spelling mistakes
    fixed spelling mistakes such as stickeScore and changed rolls to roll in the test.py as we do not want to call a list as a function.

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ class TestBowlingGame(unittest.TestCase):
 
     def testGutterGame(self):
         for i in range(0, 20):
-            self.game.rolls(0)
+            self.game.roll(0)
         assert self.game.score() == 0
 
     def testAllOnes(self):
@@ -19,16 +19,16 @@ class TestBowlingGame(unittest.TestCase):
         assert self.game.score() == 20
 
     def testOneSpare(self):
-        self.game.rolls(5)
-        self.game.rolls(5)
-        self.game.rolls(3)
+        self.game.roll(5)
+        self.game.roll(5)
+        self.game.roll(3)
         self.rollMany(0, 17)
         assert self.game.score() == 16
 
     def testOneStrike(self):
-        self.game.rolls(10)
-        self.game.rolls(4)
-        self.game.rolls(3)
+        self.game.roll(10)
+        self.game.roll(4)
+        self.game.roll(3)
         self.rollMany(0, 16)
         assert self.game.score() == 24
 
@@ -42,4 +42,4 @@ class TestBowlingGame(unittest.TestCase):
 
     def rollMany(self, pins, rolls):
         for i in range(rolls):
-            self.game.rolls(pins)
+            self.game.roll(pins)
